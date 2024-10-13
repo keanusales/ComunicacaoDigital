1e1e7b6eda8be13b5f49f070e55d5fd834747bb0
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2024-10-13 14:33:48 -0300

    Update crc.pyx

--- Atividade 4/crc.pyx
+++ Atividade 4/crc.pyx
@@ -4,13 +4,8 @@
 
 cdef extern from "crc.h" nogil:
   ctypedef unsigned uint64
-  ctypedef unsigned uint8
-  uint8 __order__"order"(uint64)
   uint64 __transfer__"transfer"(uint64, uint64)
   uint64 __receiver__"receiver"(uint64, uint64)
-
-def order(uint64 data):
-  return __order__(data)
 
 def transfer(uint64 data, uint64 gen):
   return __transfer__(data, gen)
